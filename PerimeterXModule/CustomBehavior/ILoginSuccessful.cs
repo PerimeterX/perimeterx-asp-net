b0e330dcece7bcbd5be34736aa7bb633e2bd7aa0
chen-zimmer-px <chen.zimmer@perimeterx.com>
2023-02-20 10:26:49 +0000

    add loginSuccessful interface

---
+++ PerimeterXModule/CustomBehavior/ILoginSuccessful.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PerimeterX.CustomBehavior
+{
+    public interface ILoginSuccessful
+    {
+        void Handle(HttpApplication HttpApplication, PxContext pxContext, PxModuleConfigurationSection pxConfig);
+    }
+}
